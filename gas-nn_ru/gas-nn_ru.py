7ed34ce48a5d7970a76d89e5203ef4357f6c5e9c
StepanovK <doom2good@gmail.com>
2021-04-08 22:10:06 +0300

    Сделал авторизацию

--- gas-nn_ru/gas-nn_ru.py
+++ gas-nn_ru/gas-nn_ru.py
@@ -27,36 +27,38 @@ def get_webdriver(test_mode=False):
     # if not test_mode:
     #     options.add_argument('--headless')
     current_dir = os.getcwd()
-    path = r"{}\chromedriver.exe".format(current_dir)
+    chromedriver_dir = current_dir.replace('\\gas-nn_ru', '')
+    path = r"{}\chromedriver.exe".format(chromedriver_dir)
     return webdriver.Chrome(options=options, executable_path=path)
 
 
 def log_in_mainpage(driver: webdriver, auth_settings):
     # driver = webdriver.Chrome()
     input_fam = driver.find_element_by_xpath("//td/input[@type='text'][@name='fam']")
-    print("Поле ввода фамилии: {}".format(input_fam))
     input_fam.clear()
     input_fam.send_keys(auth_settings.get('family_name'))
 
     input_account = driver.find_element_by_xpath("//td/input[@type='text'][@name='lschet'][@id='lschet']")
-    print("Поле ввода счета: {}".format(input_account))
     input_account.clear()
     input_account.send_keys(auth_settings.get('account'))
 
     select_city= driver.find_element_by_xpath("//td/select[@name='c_id']")
-    print("Поле ввода города: {}".format(select_city))
     select_city.send_keys(auth_settings.get('city_id'))
 
     buton_submit= driver.find_element_by_xpath("//td/input[@type='submit'][@name='go']")
-    print("Кнопка: {}".format(buton_submit))
     buton_submit.click()
 
 
-if __name__ == '__main__':
+def get_auth_settings(user_id=None):
     auth_settings = {
         'family_name': 'Степанов',
         'account': '051000507028',
         'city_id': 100
     }
-    send_readings(auth_settings, None)
+    return auth_settings
+
+
+if __name__ == '__main__':
+
+    send_readings(get_auth_settings(), None)
     time.sleep(10)
