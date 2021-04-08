fc71c5dce410a851f4bc588181a6eb0d4070cece
StepanovK <doom2good@gmail.com>
2021-04-08 20:42:32 +0300

    Сделал авторизацию

--- gas-nn_ru.py
+++ gas-nn_ru.py
@@ -3,11 +3,11 @@ import time
 from selenium import webdriver
 
 
-def send_reading(auth_settings, readings, test_mode=False):
+def send_readings(auth_settings, readings, test_mode=False):
     mainpage_url = get_mainpage_url(test_mode)
     wd = get_webdriver(test_mode)
-    driver = wd.get(mainpage_url)
-    log_in_mainpage(driver, auth_settings)
+    wd.get(mainpage_url)
+    log_in_mainpage(wd, auth_settings)
 
 
 def get_mainpage_url(test_mode=False):
@@ -31,13 +31,32 @@ def get_webdriver(test_mode=False):
     return webdriver.Chrome(options=options, executable_path=path)
 
 
-def log_in_mainpage(driver, auth_settings):
+def log_in_mainpage(driver: webdriver, auth_settings):
+    # driver = webdriver.Chrome()
     input_fam = driver.find_element_by_xpath("//td/input[@type='text'][@name='fam']")
-    print("Окно ввода фамилии: {}".format(input_fam))
+    print("Поле ввода фамилии: {}".format(input_fam))
+    input_fam.clear()
+    input_fam.send_keys(auth_settings.get('family_name'))
 
     input_account = driver.find_element_by_xpath("//td/input[@type='text'][@name='lschet'][@id='lschet']")
-    print("Окно ввода счета: {}".format(input_account))
+    print("Поле ввода счета: {}".format(input_account))
+    input_account.clear()
+    input_account.send_keys(auth_settings.get('account'))
 
-    select_fam = driver.find_element_by_xpath("//td/select[@name='c_id']")
-    print("Окно ввода города: {}".format(select_fam))
+    select_city= driver.find_element_by_xpath("//td/select[@name='c_id']")
+    print("Поле ввода города: {}".format(select_city))
+    select_city.send_keys(auth_settings.get('city_id'))
 
+    buton_submit= driver.find_element_by_xpath("//td/input[@type='submit'][@name='go']")
+    print("Кнопка: {}".format(buton_submit))
+    buton_submit.click()
+
+
+if __name__ == '__main__':
+    auth_settings = {
+        'family_name': 'Степанов',
+        'account': '051000507028',
+        'city_id': 100
+    }
+    send_readings(auth_settings, None)
+    time.sleep(10)
