afc37012854e92f58fc83ff1cb2981fa5c817dd2
StepanovK <doom2good@gmail.com>
2022-03-29 21:11:22 +0300

    Убрал из таблицы показаний лишнее поле "отправлено автоматически". По сути то же самое, что и просто "отправлено"

--- database/test_data.sql
+++ database/test_data.sql
@@ -7,12 +7,12 @@ INSERT INTO gas_nn_accounts (user, name, login, family_name, auto_sending)
 INSERT INTO gas_nn_accounts (user, name, login, family_name, auto_sending)
     VALUES (300925449, 'Счетчик в доме', '051000507028', 'Степанов', 1);
 
-INSERT INTO gas_nn_meter_readings (account, date, current_value, is_sent, sent_automatically, date_of_sending)
-    VALUES (2, 1627076470, 617, 1, 1, 1627249270);
+INSERT INTO gas_nn_meter_readings (account, date, current_value, is_sent, date_of_sending)
+    VALUES (2, 1627076470, 617, 1, 1627249270);
 
-INSERT INTO gas_nn_meter_readings (account, date, current_value, is_sent, sent_automatically, date_of_sending)
-    VALUES (2, 1629326470, 617, 1, 1, 1629927670);
+INSERT INTO gas_nn_meter_readings (account, date, current_value, is_sent, date_of_sending)
+    VALUES (2, 1629326470, 617, 1, 1629927670);
 
-INSERT INTO gas_nn_meter_readings (account, date, current_value, is_sent, sent_automatically, date_of_sending)
-    VALUES (2, 1632519670, 617, 0, 0, 1629928670);
+INSERT INTO gas_nn_meter_readings (account, date, current_value, is_sent, date_of_sending)
+    VALUES (2, 1632519670, 617, 0, 1629928670);
 
