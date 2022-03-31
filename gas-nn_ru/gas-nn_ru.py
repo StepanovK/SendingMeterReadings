d674b585a59249aecd668987f745034ac2271e8c
StepanovK <doom2good@gmail.com>
2022-03-31 19:37:55 +0300

    Переписал под новый сайт

--- gas-nn_ru/gas-nn_ru.py
+++ gas-nn_ru/gas-nn_ru.py
@@ -2,63 +2,53 @@ import os
 import time
 from selenium import webdriver
 
+login_page_url = 'https://www.gas-nn.ru/login'
+meter_reading_page_url = 'https://www.gas-nn.ru/lk/indication'
+
 
 def send_readings(auth_settings, readings, test_mode=False):
-    mainpage_url = get_mainpage_url(test_mode)
     wd = get_webdriver(test_mode)
-    wd.get(mainpage_url)
-    log_in_mainpage(wd, auth_settings)
-
-
-def get_mainpage_url(test_mode=False):
-    if test_mode:
-        current_dir = os.getcwd()
-        current_dir = current_dir.replace('\\', '/')
-        url = 'file://' + current_dir + '/TestPages/gas-nn_ru.html'
-    else:
-        url = 'https://www.gas-nn.ru/form.php'
-    return url
+    wd.get(login_page_url)
+    log_in(wd, auth_settings)
 
 
 def get_webdriver(test_mode=False):
     options = webdriver.ChromeOptions()
     options.add_argument('--ignore-certificate-errors')
     options.add_argument('--incognito')
-    # if not test_mode:
-    #     options.add_argument('--headless')
+    if not test_mode:
+        options.add_argument('--headless')
     current_dir = os.getcwd()
     chromedriver_dir = current_dir.replace('\\gas-nn_ru', '')
     path = r"{}\chromedriver.exe".format(chromedriver_dir)
     return webdriver.Chrome(options=options, executable_path=path)
 
 
-def log_in_mainpage(driver: webdriver, auth_settings):
-    # driver = webdriver.Chrome()
-    input_fam = driver.find_element_by_xpath("//td/input[@type='text'][@name='fam']")
-    input_fam.clear()
-    input_fam.send_keys(auth_settings.get('family_name'))
+def log_in(driver: webdriver, auth_settings):
 
-    input_account = driver.find_element_by_xpath("//td/input[@type='text'][@name='lschet'][@id='lschet']")
-    input_account.clear()
-    input_account.send_keys(auth_settings.get('account'))
+    xpath = "//div/input[@id='formControlEmail'][@type='text'][@autocomplete='email']"
+    input_login = driver.find_element_by_xpath(xpath)
+    input_login.clear()
+    input_login.send_keys(auth_settings.get('email'))
 
-    select_city= driver.find_element_by_xpath("//td/select[@name='c_id']")
-    select_city.send_keys(auth_settings.get('city_id'))
+    xpath = "//div/input[@name='ischet'][@type='password'][@autocomplete='password']"
+    input_pass = driver.find_element_by_xpath(xpath)
+    input_pass.clear()
+    input_pass.send_keys(auth_settings.get('pass'))
 
-    buton_submit= driver.find_element_by_xpath("//td/input[@type='submit'][@name='go']")
-    buton_submit.click()
+    button_submit = driver.find_element_by_class_name('btn-primary')
+    button_submit.click()
 
 
 def get_auth_settings(user_id=None):
     auth_settings = {
-        'family_name': 'Степанов',
-        'account': '051000507028',
-        'city_id': 100
+        'email': '',
+        'pass': '',
     }
     return auth_settings
 
 
 if __name__ == '__main__':
 
-    send_readings(get_auth_settings(), None)
-    time.sleep(10)
+    send_readings(get_auth_settings(), None, False)
+    time.sleep(20)
