3bbc0c76f0d636d43a3d017455815b3cc3b99165
Konstantin <doom2good@mail.ru>
2022-10-01 15:17:05 +0300

    Настроил работу через docker

--- Bot/DB_actions/commands.py
+++ Bot/DB_actions/commands.py
@@ -85,8 +85,6 @@ def reset_database():
                     WITH 
                     OWNER = postgres
                     ENCODING = 'UTF8'
-                    LC_COLLATE = 'Russian_Russia.1251'
-                    LC_CTYPE = 'Russian_Russia.1251'
                     TABLESPACE = pg_default
                     CONNECTION LIMIT = -1;"""
             )
